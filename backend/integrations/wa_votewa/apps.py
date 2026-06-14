4c12eff5e3cc0cd0dec8de08e22857374b090ebc
Your Name <you@example.com>
2026-06-13 22:11:37 -0400

    feat(wa-votewa): scaffold package + register in INSTALLED_APPS

---
+++ backend/integrations/wa_votewa/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WaVoteWaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "integrations.wa_votewa"
+    label = "wa_votewa"
+    verbose_name = "Washington VoteWA Integration"
