751fba90ad859cc7a237c30920ba718301a24f74
Your Name <you@example.com>
2026-07-14 14:01:06 -0400

    feat(tn): scaffold Tennessee SOS integration

--- backend/elections/models.py
+++ backend/elections/models.py
@@ -108,6 +108,7 @@ class Race(models.Model):
         GA_SOS = 'ga_sos', 'Georgia SOS'
         IL_SBE = 'il_sbe', 'Illinois SBE'
         MN_SOS = 'mn_sos', 'Minnesota SOS'
+        TN_SOS = 'tn_sos', 'Tennessee SOS'
         OR_SOS = 'or_sos', 'Oregon SOS'
 
     class RaceStatus(models.TextChoices):
